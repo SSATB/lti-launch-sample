1932e444df303c66116cbbb73e6e795ccc22a9a1
Kam Jim <kjim@ssat.org>
2016-06-23 16:41:04 -0400

    initial commit

---
+++ ltitest/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web.Mvc;
+using LtiLibrary.Core.Common;
+using LtiLibrary.Core.Lti1;
+using ltitest.Models;
+using Newtonsoft.Json;
+
+namespace ltitest.Controllers
+{
+    public class HomeController : Controller
+    {
+        const string consumerKey = "adfe-8d2435fbf9b.admission.org";
+        const string consumerSecret = "1uJCUiZQXCwNFHsv";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetLtiResource(string resourceUrl, string resourceId)
+        {
+            var resource = getLtiResource(resourceUrl, resourceId);
+            return Content(JsonConvert.SerializeObject(resource), "application/json");
+        }
+
+        LtiResource getLtiResource(string resourceUrl, string resourceId)
+        {
+            Uri launchUri;
+            var ltiRequest = new LtiRequest(LtiConstants.BasicLaunchLtiMessageType)
+            {
+                ConsumerKey = consumerKey,
+                ResourceLinkId = resourceId,
+                Url = Uri.TryCreate(resourceUrl, UriKind.Absolute, out launchUri) ? launchUri : null,
+
+                // Tool
+                ToolConsumerInfoProductFamilyCode = "LtiLibrary",
+                ToolConsumerInfoVersion = "1.2",
+
+                // Context - populate with the Topic that we are launching from
+                ContextId = "topic1",
+                ContextTitle = "Topic 1",
+                ContextType = LisContextType.Group,
+
+                // Instance
+                ToolConsumerInstanceGuid = "4e09-9e52-926ae4e24fa.admission.org",
+                ToolConsumerInstanceName = "SSATB Default Instance",
+                //ResourceLinkTitle = "Launch",
+                //ResourceLinkDescription = "Perform a basic LTI 1.2 launch",
+
+                // User - replace this with the current user information
+                LisPersonEmailPrimary = "jdoe@andyfmiller.com",
+                LisPersonNameFamily = "Doe",
+                LisPersonNameGiven = "Joan",
+                UserId = "1"
+            };
+            ltiRequest.SetRoles(new[] { Role.Learner });
+
+            return ltiRequest.AsLtiResource(consumerSecret);
+        }
+    }
+}
---
+++ ltitest/Models/LtiResource.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+using LtiLibrary.Core.Lti1;
+using Newtonsoft.Json.Linq;
+
+namespace ltitest.Models
+{
+    public class LtiResource
+    {
+        public string action { get; set; }
+        public string signature { get; set; }
+        public dynamic fields { get; set; }
+    }
+
+    public static class LtiResourceExtensions
+    {
+        public static LtiResource AsLtiResource(this LtiRequest ltiRequest, string consumerSecret)
+        {
+            // remove empty parameters
+            var parameters = new NameValueCollection(ltiRequest.Parameters);
+            foreach (var key in parameters.AllKeys.Where(key => string.IsNullOrWhiteSpace(parameters[key])))
+                parameters.Remove(key);
+
+            // Perform the custom variable substitutions
+            ltiRequest.SubstituteCustomVariables(parameters);
+
+            // Calculate the signature based on the substituted values
+            var signature = ltiRequest.GenerateSignature(parameters, consumerSecret);
+
+            var resource = new LtiResource
+            {
+                action = ltiRequest.Url.ToString(),
+                fields = new JObject(),
+                signature = signature
+            };
+            foreach (var key in parameters.AllKeys)
+                resource.fields[key] = parameters[key];
+
+            return resource;
+        }
+    }
+}
